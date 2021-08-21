d2a002041124ee27f1758f8b131cab48ab072d1d
Minh Khoa <adamkhoa03@gmail.com>
2021-08-22 01:11:19 +0700

    Create lang/global.php, update activated properties css

---
+++ resources/lang/en/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'users' => [
+        'list_users' => 'List Users :status',
+    ],
+    'status' => [
+        'activated' => 'Activated',
+        'deactivated' => 'Deactivated',
+    ]
+];
